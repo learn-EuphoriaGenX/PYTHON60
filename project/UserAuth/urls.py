e62f39d45156d8a9f3cdcd053a7feff9679ceae4
Rounak <rounakbasak.personal@gmail.com>
2026-04-09 15:56:41 +0530

    forget-password

--- project/UserAuth/urls.py
+++ project/UserAuth/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', views.Login, name='login'),
     path('register/', views.Register, name='register'),
-    path('logout/', views.Logout, name='logout')
+    path('logout/', views.Logout, name='logout'),
+    path('forget-password/', views.ForgetPassword, name='forget-password'),
 ]
--- project/UserAuth/views.py
+++ project/UserAuth/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from .models import OtpModel
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+import random
+import re
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -21,6 +26,12 @@ def Register(request):
         if alreadyExists:
             messages.error(request, 'Username already exists')
             return render(request, 'register.html')
+        
+        alreadyExists = User.objects.filter(email=email).exists()
+        if alreadyExists:
+            messages.error(request, 'Email already exists')
+            return render(request, 'register.html')
+        
         else:
             new_user = User.objects.create_user(username=username, email=email, password=password)
             new_user.save()
@@ -58,3 +69,60 @@ def Logout(request):
     logout(request)
     messages.success(request, 'Logged out successfully')
     return redirect('login')
+
+
+def ForgetPassword(request):
+
+    if request.method == 'POST' and 'send_otp' in request.POST:
+        username_or_email = request.POST.get('username_or_email') # can be either username or email
+        if not username_or_email:
+            messages.error(request, 'Please enter your username or email')
+            return redirect('forget-password')
+        else:
+            user = None
+            email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+            if '@' in username_or_email and re.match(email_regex, username_or_email):   
+                try:
+                    user = User.objects.get(email=username_or_email)
+                    print(user)
+                except User.DoesNotExist:
+                    messages.error(request, f'No user found with this email {username_or_email}')
+                    return redirect('forget-password')
+            else:
+                try:
+                    user = User.objects.get(username=username_or_email)
+                except User.DoesNotExist:
+                    messages.error(request, f'No user found with this username {username_or_email}')
+                    return redirect('forget-password')
+                
+        # generate OTP and send email logic here
+        otp = random.randint(1000, 9999)
+        new_otp = OtpModel(user=user, otp=otp)
+        new_otp.save()
+        # send email to user.email with the otp
+
+        try:
+            send_mail(
+                'Your OTP for Password Reset', # email subject
+                f'Your OTP for password reset is {otp}. It is valid for 10 minutes.', # email body
+                settings.DEFAULT_FROM_EMAIL, # from email
+                [user.email], # to email
+                fail_silently=False, # in case of error, it will raise an exception instead of failing silently
+            )
+        except Exception as e:
+                messages.error(request, f'Error sending email: {str(e)}')
+                return redirect('forget-password')
+        
+        messages.success(request, f'An OTP has been sent to your email {user.email}')
+        return render(request, 'submit-otp.html', {'user': user})
+
+    if request.method == 'POST' and 'resend_otp' in request.POST:
+        pass
+
+    if request.method == 'POST' and 'submit_otp' in request.POST:
+        pass
+
+    if request.method == 'POST' and 'change_password' in request.POST:
+        pass
+
+    return render(request, 'forgetpassword.html')
